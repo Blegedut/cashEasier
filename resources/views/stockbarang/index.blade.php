f51172ec0244536e97325c87ec6ddf1ebfca0101
Blegedut <blegedut07@gmail.com>
2022-08-25 12:04:50 +0700

    25/08/22

--- resources/views/stockbarang/index.blade.php
+++ resources/views/stockbarang/index.blade.php
@@ -8,7 +8,54 @@
         <div class="col-6 col-lg-2 col-md-6">
             <div class="card">
                 <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted font-semibold">Nama Product</h5>
+                        <p class="mt-3">
+                            <a class="collapsed" data-bs-toggle="collapse" href="#collapseExample2" role="button"
+                                aria-expanded="false" aria-controls="collapseExample">
+                                Deskripsi
+                            </a>
+                        </p>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
+                        <div class="collapse mt-3" id="collapseExample2" style="">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
+                            exercitationem soluta eos dolorum ipsum quasi. Harum molestiae aut officia reiciendis, dicta
+                            maiores. Accusantium necessitatibus dolorum qui corporis.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-lg-2 col-md-6">
+            <div class="card">
+                <div class="card-content">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted font-semibold">Nama Product</h5>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
+                        <p class="mt-3">
+                            <a class="collapsed" data-bs-toggle="collapse" href="#collapseExample3" role="button"
+                                aria-expanded="false" aria-controls="collapseExample">
+                                Deskripsi
+                            </a>
+                        </p>
+                        <div class="collapse mt-3" id="collapseExample3" style="">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
+                            exercitationem soluta eos dolorum ipsum quasi.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-lg-2 col-md-6">
+            <div class="card">
+                <div class="card-content">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
                     <div class="card-body">
                         <h5 class="card-title text-muted font-semibold">Nama Product</h5>
                         <h6 class="card text font-extrabold mb-0">
@@ -32,89 +79,23 @@
         <div class="col-6 col-lg-2 col-md-6">
             <div class="card">
                 <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
                     <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
-                            exercitationem soluta eos dolorum ipsum quasi. Harum moles
+                        <h5 class="card-title text-muted font-semibold">Nama Product</h5>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
+                        <p class="mt-3">
+                            <a class="collapsed" data-bs-toggle="collapse" href="#collapseExample" role="button"
+                                aria-expanded="false" aria-controls="collapseExample">
+                                Deskripsi
+                            </a>
                         </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-lg-2 col-md-6">
-            <div class="card">
-                <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
-                    <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
-                            Lorem
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-lg-2 col-md-6">
-            <div class="card">
-                <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
-                    <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
-                            exercitationem soluta eos dolorum ipsum quasi. Harum molestiae aut officia reiciendis, dicta
-
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-lg-2 col-md-6">
-            <div class="card">
-                <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
-                    <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
-                            exercitationem soluta eos dolorum ipsum quasi. Harum molestiae aut officia reiciendis
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-lg-2 col-md-6">
-            <div class="card">
-                <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
-                    <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-lg-2 col-md-6">
-            <div class="card">
-                <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
-                    <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
+                        <div class="collapse mt-3" id="collapseExample" style="">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
                             exercitationem soluta eos dolorum ipsum quasi. Harum molestiae aut officia reiciendis, dicta
                             maiores. Accusantium necessitatibus dolorum qui corporis.
-                        </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -122,15 +103,62 @@
         <div class="col-6 col-lg-2 col-md-6">
             <div class="card">
                 <div class="card-content">
-                    <img src="assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid" alt="singleminded">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
                     <div class="card-body">
-                        <h5 class="card-title">Nama Product</h5>
-                        <p class="card-text">
-                            Deskirpsi
+                        <h5 class="card-title text-muted font-semibold">Nama Product</h5>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
+                        <p class="mt-3">
+                            <a class="collapsed" data-bs-toggle="collapse" href="#collapseExample" role="button"
+                                aria-expanded="false" aria-controls="collapseExample">
+                                Deskripsi
+                            </a>
+                        </p>
+                        <div class="collapse mt-3" id="collapseExample" style="">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint amet distinctio
                             exercitationem soluta eos dolorum ipsum quasi. Harum molestiae aut officia reiciendis, dicta
                             maiores. Accusantium necessitatibus dolorum qui corporis.
-                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-lg-2 col-md-6">
+            <div class="card">
+                <div class="card-content">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Product</h5>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-lg-2 col-md-6">
+            <div class="card">
+                <div class="card-content">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Product</h5>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-lg-2 col-md-6">
+            <div class="card">
+                <div class="card-content">
+                    <img src="assets/images/samples/helix-hx5.jpeg" class="card-img-top img-fluid" alt="singleminded">
+                    <div class="card-body">
+                        <h5 class="card-title">Nama Product</h5>
+                        <h6 class="card text font-extrabold mb-0">
+                            123
+                        </h6>
                     </div>
                 </div>
             </div>
