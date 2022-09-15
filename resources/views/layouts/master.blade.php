9f68b2a0d1e621d1a5585122a4dcdd5e82a0b84b
Blegedut <blegedut07@gmail.com>
2022-09-15 12:05:53 +0700

    update 15/09/2022

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -112,7 +112,7 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2021</p>
+                        <p>2022</p>
                     </div>
                     <div class="float-end">
                         <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
